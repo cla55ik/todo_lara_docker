93bc452b8682984f83e6cb68dee3d6f8adb53de0
i.chelnokov <chelnokov.ia@parmalogica.ru>
2022-11-22 16:25:06 +0300

    Session test isDone

--- tests/Feature/SessionServiceTest.php
+++ tests/Feature/SessionServiceTest.php
@@ -15,12 +15,33 @@ class SessionServiceTest extends TestCase
      *
      * @return void
      */
-    public function test_api_session_response()
+    public function test_web_session_get(): void
     {
         $response = $this->withoutExceptionHandling()->get('/test/session/');
         $response->assertStatus(200);
 
         $response = $this->get('/test/session/asd');
-        $response->assertStatus(404);
+        $response->assertStatus(200);
+    }
+
+    public function test_web_session_post(): void
+    {
+        $response = $this->post('/test/session/', [
+            'name' => 'session1',
+            'data' => 'dataSession'
+        ]);
+        $response->assertStatus(200);
+    }
+
+    public function test_web_set_session(): void
+    {
+        $response = $this->post('/test/session/', [
+            'name' => 'session1',
+            'data' => 'dataSession'
+        ]);
+        $response
+            ->assertSessionHas('session1')
+            ->assertContent('"dataSession"')
+        ;
     }
 }
