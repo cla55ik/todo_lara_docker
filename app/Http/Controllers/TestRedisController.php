698aef06dea34fd8f3fcf1bb8be36117305b0b5a
i.chelnokov <ivancheclnokov@gmail.com>
2022-12-08 15:13:06 +0300

    need to redis.conf

--- app/Http/Controllers/TestRedisController.php
+++ app/Http/Controllers/TestRedisController.php
@@ -3,15 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Response;
+use Illuminate\Support\Env;
 use Illuminate\Support\Facades\Redis;
 
 class TestRedisController
 {
     public function redisTest(): Response
     {
-        Redis::set('name', 'Taylor');
-
-        $values = Redis::lrange('names', 5, 10);
-        dd(Redis::get('name'));
+        Redis::connection()->setName('ivan');
+        Redis::connection()->client()->set('test:ivan', 'ivan');
+        dd('asd');
     }
 }
